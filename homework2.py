5d059b13013bd3443667474ddebeb7ffc96efdce
Hallerus <42898747+Hallerus@users.noreply.github.com>
2024-06-25 23:30:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+number_of_completed_work = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_to_complete_course = number_of_hours_spent / number_of_completed_work
+print('Курс: ' + course_name + ', всего задач: ' + str(number_of_completed_work) + ', затрачено часов:' + str(number_of_hours_spent) + ', среднее время выполнения ' + str(time_to_complete_course) + ' часа')
+
+print('Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.')
