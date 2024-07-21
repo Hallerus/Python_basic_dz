bd18442f908283b36711579ef2a3e049ea5154b1
Hallerus <42898747+Hallerus@users.noreply.github.com>
2024-07-21 15:21:44 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -7,6 +7,13 @@ print_params(3,4)
 print_params(c='Football')
 print_params(b=25)
 print_params(c=[1, 2, 3])
+print('')
+
+value_list = ['1.0.str', 234, True]
+value_dict = {'a':2, 'b':'снова строка', 'c':[3,5,6]}
+print_params(*value_list)
+print_params(**value_dict)
+print('')
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
